1b82b3a5f79b1648ea40f56d820b997f124e50f6
Renan Lima <renan.limagomes@gmail.com>
2022-09-15 14:31:21 -0300

    Implementação do Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
+  message = "";
+
+  loginForm = new FormGroup({
+    username: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required]),
+  });
+
+
   hide = true;
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {  }
+
+  onSucessLogin(){
+    console.log(">>>> usuário autenticado com sucesso")
+  }
+  onErrorLogin(message: any){
+    this.message = message;
+  }
+
+  onLogin(){
+    const username: string = this.loginForm.value.username || "";
+    const password: string = this.loginForm.value.password || "";
+    this.loginService.Authenticate(username, password).subscribe(() => {
+      this.onSucessLogin()
+    },(res) => {
+      this.onErrorLogin(res.error.message)
+    })
+  }
 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Product} from '../products';
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class LoginService {
+    url = 'https://localhost:7038/Api/Usuario/login';
+
+    constructor(
+        private http: HttpClient
+      ) { }
+      
+
+    public Authenticate(username: any, password: any){        
+        const body = {
+            username,
+            password
+        }
+        return this.http.post(this.url, body, {observe: 'response'})
+    }
+  }
